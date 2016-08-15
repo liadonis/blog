c965d464cb4a2a56fd31dc6efb395eab80f12eed
liadonis <li.adonis@gmail.com>
2016-08-15 22:12:57 +0800

    show data from background

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -23,15 +23,14 @@
     <div class="template">
         <div class="box">
             <h3>
-                <p><span>個人部落格</span>模板 Templates</p>
+                <p><span>站長推薦  </span>Recommend</p>
             </h3>
             <ul>
-                <li><a href="/"  target="_blank"><img src="images/01.jpg"></a><span>仿部落格風格·古典個人部落格模板</span></li>
-                <li><a href="/" target="_blank"><img src="images/02.jpg"></a><span>黑色質感時間軸html5個人部落格模板</span></li>
-                <li><a href="/"  target="_blank"><img src="images/03.jpg"></a><span>Green綠色小清新的夏天-個人部落格模板</span></li>
-                <li><a href="/" target="_blank"><img src="images/04.jpg"></a><span>女生清新個人部落格網站模板</span></li>
-                <li><a href="/"  target="_blank"><img src="images/02.jpg"></a><span>黑色質感時間軸html5個人部落格模板</span></li>
-                <li><a href="/"  target="_blank"><img src="images/03.jpg"></a><span>Green綠色小清新的夏天-個人部落格模板</span></li>
+                @foreach($hot as $k=>$v)
+
+                    <li><a href="{{url('a/'.$v->art_id)}}"  target="_blank"><img src="{{url($v->art_thumb)}}"></a><span>{{$v->art_title}}</span></li>
+
+                @endforeach
             </ul>
         </div>
     </div>
@@ -40,41 +39,20 @@
             <p>文章<span>推薦</span></p>
         </h2>
         <div class="bloglist left">
-            <h3>程是設計師請放下你的技術情節，與你的同伴一起進步</h3>
-            <figure><img src="images/001.png"></figure>
+            @foreach($data as $m=>$n)
+            <h3>{{$n->art_title}}</h3>
+            <figure><img src="{{url($n->art_thumb)}}"></figure>
             <ul>
-                <p>如果說掌握一門賴以生計的技術是技術人員要學會的第一課的話， 那麼我覺得技術人員要真正學會的第二課，不是技術，而是業務、交流與協作，學會關心其他工作夥伴的工作情況和進展...</p>
-                <a title="/" href="/" target="_blank" class="readmore">閱讀全文>></a>
+                <p>{{$n->art_description}}</p>
+                <a title="{{$n->art_title}}" href="{{url('a/'.$n->art_id)}}" target="_blank" class="readmore">閱讀全文>></a>
             </ul>
-            <p class="dateview"><span>2013-11-04</span><span>作者：</span><span>個人部落格：[<a href="/news/life/">程序人生</a>]</span></p>
-            <h3>程序员请放下你的技术情节，与你的同伴一起进步</h3>
-            <figure><img src="images/001.png"></figure>
-            <ul>
-                <p>如果说掌握一门赖以生计的技术是技术人员要学会的第一课的话， 那么我觉得技术人员要真正学会的第二课，不是技术，而是业务、交流与协作，学会关心其他工作伙伴的工作情况和进展...</p>
-                <a title="/" href="/" target="_blank" class="readmore">閱讀全文>></a>
-            </ul>
-            <p class="dateview"><span>2013-11-04</span><span>作者：后盾</span><span>个人博客：[<a href="/news/life/">程序人生</a>]</span></p>
-            <h3>程序员请放下你的技术情节，与你的同伴一起进步</h3>
-            <figure><img src="images/001.png"></figure>
-            <ul>
-                <p>如果说掌握一门赖以生计的技术是技术人员要学会的第一课的话， 那么我觉得技术人员要真正学会的第二课，不是技术，而是业务、交流与协作，学会关心其他工作伙伴的工作情况和进展...</p>
-                <a title="/" href="/" target="_blank" class="readmore">阅读全文>></a>
-            </ul>
-            <p class="dateview"><span>2013-11-04</span><span>作者：后盾</span><span>个人博客：[<a href="/news/life/">程序人生</a>]</span></p>
-            <h3>程序员请放下你的技术情节，与你的同伴一起进步</h3>
-            <figure><img src="images/001.png"></figure>
-            <ul>
-                <p>如果说掌握一门赖以生计的技术是技术人员要学会的第一课的话， 那么我觉得技术人员要真正学会的第二课，不是技术，而是业务、交流与协作，学会关心其他工作伙伴的工作情况和进展...</p>
-                <a title="/" href="/" target="_blank" class="readmore">阅读全文>></a>
-            </ul>
-            <p class="dateview"><span>2013-11-04</span><span>作者：后盾</span><span>个人博客：[<a href="/news/life/">程序人生</a>]</span></p>
-            <h3>程序员请放下你的技术情节，与你的同伴一起进步</h3>
-            <figure><img src="images/001.png"></figure>
-            <ul>
-                <p>如果说掌握一门赖以生计的技术是技术人员要学会的第一课的话， 那么我觉得技术人员要真正学会的第二课，不是技术，而是业务、交流与协作，学会关心其他工作伙伴的工作情况和进展...</p>
-                <a title="/" href="/" target="_blank" class="readmore">阅读全文>></a>
-            </ul>
-            <p class="dateview"><span>2013-11-04</span><span>作者：后盾</span><span>个人博客：[<a href="/news/life/">程序人生</a>]</span></p>
+            <p class="dateview"><span>{{date('Y-m-d',$n->art_time)}}</span><span>作者：{{$n->art_editor}}</span></p>
+            @endforeach
+            <div class="page">
+
+                {{$data->links()}}
+
+            </div>
         </div>
         <aside class="right">
             <div class="weather"><iframe width="250" scrolling="no" height="60" frameborder="0" allowtransparency="true" src="http://i.tianqi.com/index.php?c=code&id=12&icon=1&num=1"></iframe></div>
